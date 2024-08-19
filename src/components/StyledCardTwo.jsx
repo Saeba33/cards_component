e59b064952b662d00fa91c9d25f18b400bcf2f43
Charles Proust <konoha33@hotmail.fr>
2024-08-19 19:37:19 +0200

    styled card one and two

--- src/components/StyledCardTwo.jsx
+++ src/components/StyledCardTwo.jsx
@@ -1,45 +1,44 @@
 import Image from "next/image";
 import links from "@/data/StyledCardTwoData";
 
-
 export default function StyledCardTwo() {
   return (
-    <div className="min-h-screen w-full bg-red-800 relative flex items-center justify-center">
-      <div className="absolute w-[500px] h-[400px]">
-        <div className="w-full h-full overflow-hidden  group">
+    <div className="min-h-screen w-full bg-red-800 flex items-center justify-center relative">
+      <div className="relative w-[500px] h-[400px] group">
+        {/* Image */}
+        <div className="w-full h-full overflow-hidden relative">
           <Image
             src="/1.jpg"
             alt="picture"
-            width={100}
-            height={100}
-            className="w-full h-full duration-500 hover:opacity-50 group-hover:translate-x-9"
+            width={500}
+            height={400}
+            className="w-full h-full object-cover duration-500 group-hover:opacity-50 group-hover:translate-x-9"
           />
-          <div className="absolute top-0 left-0 w-[70%] h-full bg-[#ffc107] transition-transform duration-500 perspective group-hover:rotate-hover">
-            <div className="absolute top-1/4 w-full p-5 text-center bg-white">
-              <h3 className="text-red-500 text-3xl font-semibold">Title</h3>
-              <span className="text-gray-800 text-xl font-medium">
-                Subtitle
-              </span>
-              <p className="text-gray-800">
-                Lorem ipsum is simple dummy text on the printing and typesetting
-                industry.
-              </p>
-              <ul className="flex justify-center space-x-2 mt-2">
-                {links.map((link, index) => (
-                  <li key={index}>
-                    <a
-                      href={link.href}
-                      className="flex w-[30px] h-[30px] bg-gray-800 text-white items-center justify-center transition-all duration-500 hover:bg-red-500"
-                      aria-label={link.ariaLabel}
-                    >
-                      <i className="text-center" aria-hidden="true">
-                        {link.text}
-                      </i>
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+        </div>
+        {/* Détails */}
+        <div className="absolute top-0 left-0 w-[70%] h-full bg-[#ffc107] perspective  duration-500 group-hover:rotate-hover group-hover:opacity-100 opacity-0">
+          <div className="absolute top-1/4 p-5 text-center bg-white">
+            <h3 className="text-red-500 text-3xl font-semibold">Title</h3>
+            <span className="text-gray-800 text-xl font-medium">Subtitle</span>
+            <p className="text-gray-800">
+              Lorem ipsum is simple dummy text on the printing and typesetting
+              industry.
+            </p>
+            <ul className="flex justify-center space-x-2 mt-2">
+              {links.map((link, index) => (
+                <li key={index}>
+                  <a
+                    href={link.href}
+                    className="flex w-[30px] h-[30px] bg-gray-800 text-white items-center justify-center transition-all duration-500 hover:bg-red-500"
+                    aria-label={link.ariaLabel}
+                  >
+                    <i className="text-center" aria-hidden="true">
+                      {link.text}
+                    </i>
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
